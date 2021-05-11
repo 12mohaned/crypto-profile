6519236d28b6da893bdf087b91beec3eaff2ded0
mohaned-mashaly <mohaned_boss@outlook.com>
2021-05-11 21:20:44 +0200

    increase and decrease indicator

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,13 +2,15 @@ require 'http'
 require 'json'
 class HomesController < ApplicationController
     def home
+        @featured_index = 0
+        @price_change   = Array.new
         @featured_coins = Hash.new
         @featured_coins["Bitcoin"]  =  convert_json("BTCUSDT")
         @featured_coins["Ethereum"]  =  convert_json("ETHUSDT")
         @featured_coins["Dash"] = convert_json("DASHUSDT")
         @featured_coins["Doge"] = convert_json("DOGEUSDT")
         @featured_coins["XRP"] =  convert_json("XRPUSDT")  
-
+        calculate_avg_price(@featured_coins, 5)
         @Currencies = HTTP.get("https://api.binance.com/api/v3/ticker/price?").body.to_s
         @Currencies = JSON.parse(@Currencies)
         @Currencies_hash = {}
@@ -28,6 +30,22 @@ class HomesController < ApplicationController
         @currency = HTTP.get("https://api.binance.com/api/v3/ticker/price?symbol="+symbol).body.to_s
         @currency = JSON.parse(@currency)
         return @currency
+    end
+    
+    #Calculate the change in price and determine whether featured prices increased or decreased
+    def calculate_avg_price(featured_coins, num)
+        $i = 0
+        $num = 5
+        @prices_change = Array.new
+        @featured_coins.each do |name, symbol|
+        @stocks = HTTP.get("https://api.binance.com/api/v3/ticker/24hr?symbol=" + symbol['symbol']).body.to_s
+        @stocks = JSON.parse(@stocks)
+        if @stocks['priceChangePercent'].to_f > 0
+            @prices_change.push(true) 
+        else
+            @prices_change.push(false)
+        end
+    end
     end
 
 end
